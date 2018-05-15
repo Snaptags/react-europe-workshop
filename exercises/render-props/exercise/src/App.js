da3cef950ba672da00dc4697570e6090cf77976e
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-15 10:15:35 +0200

    better approach: avoid using arrow function inside of render()

--- exercises/render-props/exercise/src/App.js
+++ exercises/render-props/exercise/src/App.js
@@ -4,17 +4,16 @@ import './App.css';
 import Fetch from './Fetch';
 
 class App extends Component {
+    renderFetch = ({loading, error, data}) => {
+        if (error !== null) {
+            return <p>Error: {error}</p>;
+        }
+        return loading === true ? <p> Loading...</p> : <p>{data}</p>
+    };
     render() {
         return (
-            <Fetch url="http://test.com" >
-            {({loading, error, data}) => {
-                if (error !== null) {
-                    return <p>Error: {error}</p>;
-                }
-                return loading === true ? <p> Loading...</p> : <p>{data}</p>
-            }}
-            </Fetch>
-    );
+            <Fetch url="http://test.com" children={this.renderFetch} />
+        );
     }
 }
 
