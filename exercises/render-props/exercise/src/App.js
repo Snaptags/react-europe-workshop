55c8465dea26f45fec0f08e940aba24195ded4af
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-15 10:12:06 +0200

    first exercise: render props

--- exercises/render-props/exercise/src/App.js
+++ exercises/render-props/exercise/src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 import Fetch from './Fetch';
 
 class App extends Component {
-  render() {
-    return <Fetch />;
-  }
+    render() {
+        return (
+            <Fetch url="http://test.com" >
+            {({loading, error, data}) => {
+                if (error !== null) {
+                    return <p>Error: {error}</p>;
+                }
+                return loading === true ? <p> Loading...</p> : <p>{data}</p>
+            }}
+            </Fetch>
+    );
+    }
 }
 
 export default App;
--- exercises/render-props/exercise/src/Fetch.js
+++ exercises/render-props/exercise/src/Fetch.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
+const fakeFetch = url => new Promise((resolve, reject) => { // should normally fetch stuff from a server or middleware whatever
+    setTimeout(() => {
+        //reject('Oh, NO');
+        resolve('HEY');
+    }, 1000);
+});
+
 class Fetch extends Component {
-  render() {
-    return <p>Fetch Component</p>;
-  }
+    state = {
+        loading: true, // won't have data when rendering for the first time
+        error: null,
+        data: null,
+    };
+    componentDidMount() {
+        fakeFetch(this.props.url)
+            .then(data => {
+                this.setState({
+                    data,
+                    loading: false,
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    error: err,
+                    loading: false
+                });
+            });
+    }
+    render() {
+        return this.props.children(this.state);
+    }
 }
 
 export default Fetch;
