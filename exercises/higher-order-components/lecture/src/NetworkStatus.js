478e11f939857b3d8e858788bc46add89242eead
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-15 14:27:48 +0200

    refactor to HOC; todo: pass refs

--- exercises/higher-order-components/lecture/src/NetworkStatus.js
+++ exercises/higher-order-components/lecture/src/NetworkStatus.js
@@ -1,68 +1,75 @@
 import React, { Component } from "react";
+import hoistStatics from "hoist-non-react-statics";
 
-export class Online extends Component {
-  static ConnectionSpeed = () => {
-    const { downlink } = navigator.connection;
-    return <span>~{downlink}Mbp/s</span>;
-  };
-
-  static ConnectionType = () => {
-    const { effectiveType } = navigator.connection;
-    return <span>{effectiveType.toUpperCase()}</span>;
-  };
-  state = {
-    isOnline: navigator.onLine
-  };
-
-  componentDidMount() {
-    window.addEventListener("online", this.handleOnline);
-    window.addEventListener("offline", this.handleOffline);
-  }
-
-  componentWillUnount() {
-    window.removeEventListener("online", this.handleOnline);
-    window.removeEventListener("offline", this.handleOffline);
-  }
-
-  handleOnline = () => {
-    this.setState({ isOnline: true });
-  };
-
-  handleOffline = () => {
-    this.setState({ isOnline: false });
-  };
-
-  render() {
-    const { isOnline } = this.state;
-    return isOnline ? this.props.children : null;
-  }
+function getDisplayName(C) {
+    return C.displayName || C.name || C
 }
+function withOnlineStatus(
+    WrappedComponent
+) {
+    class OnlineStatus extends Component {
+        state = {
+            isOnline: navigator.onLine
+        };
 
-export class Offline extends Component {
-  state = {
-    isOnline: navigator.onLine
-  };
+        componentDidMount() {
+            window.addEventListener("online", this.handleOnline);
+            window.addEventListener("offline", this.handleOffline);
+        }
 
-  componentDidMount() {
-    window.addEventListener("online", this.handleOnline);
-    window.addEventListener("offline", this.handleOffline);
+        componentWillUnount() {
+            window.removeEventListener("online", this.handleOnline);
+            window.removeEventListener("offline", this.handleOffline);
+        }
+
+        handleOnline = () => {
+            this.setState({ isOnline: true });
+        };
+
+        handleOffline = () => {
+            this.setState({ isOnline: false });
+        };
+
+        render() {
+            return (
+                <WrappedComponent
+                    isOnline = {this.state.isOnline}
+                    {...this.props}
+                />
+            );
+        }
+    }
+    hoistStatics(OnlineStatus, WrappedComponent);
+    // extend displayName in order to get a more transparent output in the react devtools:
+    const displayName = getDisplayName(WrappedComponent);
+    OnlineStatus.displayName = `withOnlineStatus(${displayName})`;
+    return OnlineStatus;
+};
+
+
+export const Online = withOnlineStatus(
+    class Online extends Component {
+        static ConnectionSpeed = () => {
+            const { downlink } = navigator.connection;
+            return <span>~{downlink}Mbp/s</span>;
+        };
+
+        static ConnectionType = () => {
+            const { effectiveType } = navigator.connection;
+            return <span>{effectiveType.toUpperCase()}</span>;
+        };
+        render() {
+            const { isOnline } = this.props;
+            return isOnline ? this.props.children : null;
+        }
+    }
+);
+
+export const Offline = withOnlineStatus(
+  class Offline extends Component {
+    render() {
+      const { isOnline, children } = this.props;
+      return isOnline ? null : children;
+    }
   }
-
-  componentWillUnount() {
-    window.removeEventListener("online", this.handleOnline);
-    window.removeEventListener("offline", this.handleOffline);
-  }
-
-  handleOnline = () => {
-    this.setState({ isOnline: true });
-  };
-
-  handleOffline = () => {
-    this.setState({ isOnline: false });
-  };
-
-  render() {
-    const { isOnline } = this.state;
-    return isOnline ? null : this.props.children;
-  }
-}
+);
