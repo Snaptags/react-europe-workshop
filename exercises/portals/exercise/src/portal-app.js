c6ea27cea60b5dc7d0e327e936cab9c2187f54d7
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-15 16:11:25 +0200

    should show the popup below the button, but does not

--- exercises/portals/exercise/src/portal-app.js
+++ exercises/portals/exercise/src/portal-app.js
@@ -18,52 +18,68 @@ export class PortalModal extends Component {
     }
 
     render() {
-        return ReactDOM.createPortal(<div className="regularModal">Hi I'm a regular modal!!</div>, this.portalElement)
+        const { coordinates } = this.props;
+        return ReactDOM.createPortal(<div className="regularModal" style={{ left: `${coordinates.x}px, top:${coordinates.y}px`}}>Hi I'm a regular modal!!</div>, this.portalElement)
     }
 }
 
 export class PortalApp extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      isModalOpen: false
+        this.state = {
+            isModalOpen: false,
+            modalCoordinates: {},
+        };
+    }
+
+    getModalCoordinates = () => {
+        if (this.buttonWrapper) {
+            const boundingRect = this.buttonWrapper.getBoundingClientRect();
+            this.setState({
+                modalCoordinates: {
+                    x: boundingRect.left,
+                    y: boundingRect.bottom + 10,
+                }});
+        }
+    }
+
+    toggleModal = () => {
+        if (!this.state.isModalOpen) {
+            this.getModalCoordinates();
+        }
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
     };
-  }
 
-  toggleModal = () => {
-    this.setState({
-      isModalOpen: !this.state.isModalOpen
-    });
-  };
-
-  render() {
-    return (
-      <div className="pageWrapper">
-        <div className={`containerDiv lightTheme`}>
-          <h1>Portals!!!!</h1>
-          <div className="settingsButtonWrapper">
-            <button className="settingsButton" onClick={this.toggleModal}>
-              settings
-            </button>
-            {this.state.isModalOpen ? <PortalModal /> : null}
-          </div>
-          <p>Portals are great!</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-          <p>Here is some more content about portals</p>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="pageWrapper">
+                <div className={`containerDiv lightTheme`}>
+                    <h1>Portals!!!!</h1>
+                    <div className="settingsButtonWrapper" ref={el=> {this.buttonWrapper}}>
+                        <button className="settingsButton" onClick={this.toggleModal}>
+                            settings
+                        </button>
+                        {this.state.isModalOpen ? <PortalModal coordinates={this.state.modalCoordinates}/> : null}
+                    </div>
+                    <p>Portals are great!</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                    <p>Here is some more content about portals</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default PortalApp;
