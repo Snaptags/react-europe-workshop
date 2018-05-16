e17ae02522d4b23869954db7ad5631468cb72030
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-16 10:14:22 +0200

    add the checkboxes

--- exercises/form-management/exercise/src/formik-form.js
+++ exercises/form-management/exercise/src/formik-form.js
@@ -33,7 +33,7 @@ export default class FormikForm extends Component {
   // we'd also have to have several onChange handlers and, of course, a more useful submit method!
 
   render() {
-      console.log(this.state.submissions);
+    const technologies = ["react", "formik", "graphQL", "reactNative"];
     return (
       <div className="pageWrapper">
         <div className="formWrapper">
@@ -57,63 +57,24 @@ export default class FormikForm extends Component {
                             value={values.name}
                             required
                         />
+                        <p>Which of the following technologies have you used?</p>
+                        {technologies.map(
+                            item => <label><Field
+                                type="checkbox"
+                                id={item}
+                                label={item}
+                                name={item}
+                            />
+                            {item.replace(/^(.)/, $1 => $1.toUpperCase() )}
+                        </label>
+                        )}
+
                         <button className="submitButton" type="submit">
                             Submit Form
                         </button>
                     </Form>
                 )}
             />
-            {/*<form className="form" onSubmit={this.handleSubmit}>
-            <div>
-              <label htmlFor="name">What's your name?</label>
-              <input type="text" id="name" name="name" />
-            </div>
-            <div>
-              <p>Which of the following technologies have you used?</p>
-              <div>
-                <input type="checkbox" id="react" name="react" value="react" />
-                <label htmlFor="react">React</label>
-              </div>
-              <div>
-                <input type="checkbox" id="redux" name="redux" value="redux" />
-                <label htmlFor="redux">Redux</label>
-              </div>
-              <div>
-                <input
-                  type="checkbox"
-                  id="formik"
-                  name="formik"
-                  value="formik"
-                />
-                <label htmlFor="formik">Formik</label>
-              </div>
-              <div>
-                <input
-                  type="checkbox"
-                  id="graphQL"
-                  name="graphQL"
-                  value="graphQL"
-                />
-                <label htmlFor="graphQL">GraphQL</label>
-              </div>
-              <div>
-                <input type="checkbox" id="node" name="node" value="node" />
-                <label htmlFor="node">Node</label>
-              </div>
-              <div>
-                <input
-                  type="checkbox"
-                  id="reactNative"
-                  name="reactNative"
-                  value="reactNative"
-                />
-                <label htmlFor="reactNative">ReactNative</label>
-              </div>
-            </div>
-            <button className="submitButton" type="submit">
-              Submit Form
-            </button>
-          </form> */}
         </div>
         <Content submissions={this.state.submissions} />
       </div>
