07aec9c39b3d27fcb6814c163090e4bb4d36e758
Markus Lasermann <markus.lasermann@ptvgroup.com>
2018-05-16 15:26:21 +0200

    things to remember when updating state

--- exercises/set-state/lecture/src/App.js
+++ exercises/set-state/lecture/src/App.js
@@ -7,10 +7,30 @@ class App extends Component {
   };
 
   increment = () => {
+      console.log('before: ', this.state.count);
     this.setState({
       count: this.state.count + 1,
     });
+      console.log('after: ', this.state.count); // won't update → setState is asynchronous!
+      // BUT: will be synchronous when called inside of a setTimeout block!
+      // i.e. don't rely on either behaviour
   };
+
+    increment_multiple_times = () => {
+        // use this form if the state update depends on a previous state!
+        this.setState(state => {
+            return {
+                count: state.count + 1,
+            }
+        });
+    };
+
+    incrementBy = (n, fn) => {
+        let i=0;
+        while (i++ < n) {
+            fn();
+        }
+    };
 
   render() {
     const {count} = this.state;
@@ -21,6 +41,16 @@ class App extends Component {
           className="btn"
           onClick={this.increment}>
           +
+        </button>
+        <button
+          className="btn"
+          onClick={() => this.incrementBy(5, this.increment)}>
+          +5 (won't work)
+        </button>
+        <button
+          className="btn"
+          onClick={() => this.incrementBy(5, this.increment_multiple_times)}>
+          +5
         </button>
       </div>
     );
